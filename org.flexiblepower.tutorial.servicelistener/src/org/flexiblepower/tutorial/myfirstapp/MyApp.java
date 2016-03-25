c2540fc23cd22d04079218e77526b6e3382186fb
jpwijbenga@tno.nl <jpwijbenga@tno.nl>
2016-03-25 16:38:27 +0100

    First commit

---
+++ org.flexiblepower.tutorial.servicelistener/src/org/flexiblepower/tutorial/myfirstapp/MyApp.java
@@ -0,0 +1,50 @@
+package org.flexiblepower.tutorial.myfirstapp;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+
+public class MyApp implements BundleActivator, ServiceListener {
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+        System.out.println("Starting to listen for service events.");
+        context.addServiceListener(this);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+        context.removeServiceListener(this);
+        System.out.println("Stopped listening for service events.");
+	}
+
+    /**
+     * Implements ServiceListener.serviceChanged().
+     * Prints the details of any service event from the framework.
+     * @param event the fired service event.
+    **/
+	@Override
+    public void serviceChanged(ServiceEvent event)
+    {
+        String[] objectClass = (String[])
+            event.getServiceReference().getProperty("objectClass");
+
+        if (event.getType() == ServiceEvent.REGISTERED)
+        {
+            System.out.println(
+                "Ex1: Service of type " + objectClass[0] + " registered.");
+        }
+        else if (event.getType() == ServiceEvent.UNREGISTERING)
+        {
+            System.out.println(
+                "Ex1: Service of type " + objectClass[0] + " unregistered.");
+        }
+        else if (event.getType() == ServiceEvent.MODIFIED)
+        {
+            System.out.println(
+                "Ex1: Service of type " + objectClass[0] + " modified.");
+        }
+    }
+	
+}
